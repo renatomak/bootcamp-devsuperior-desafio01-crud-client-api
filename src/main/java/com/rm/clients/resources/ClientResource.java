b90c861cc6a91b7d87ac440de9d9c899642ada4a
renatomak <renato.mark.silva@gmail.com>
2022-03-19 18:48:34 -0300

    feat: client resource get by id

--- src/main/java/com/rm/clients/resources/ClientResource.java
+++ src/main/java/com/rm/clients/resources/ClientResource.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +33,10 @@ public class ClientResource {
         Page<ClientDto> list = clientService.findAllPaged(pageRequest);
         return ResponseEntity.ok(list);
     }
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDto> findById(@PathVariable Long id) {
+		return ResponseEntity.ok(clientService.findById(id));
+	}
 
 }
