9a2c61d79aa1331fae057ef04afc807681014a96
renatomak <renato.mark.silva@gmail.com>
2022-03-19 16:47:50 -0300

    first commit

---
+++ src/main/java/com/rm/crudclientsapi/CrudClientsApiApplication.java
@@ -0,0 +1,13 @@
+package com.rm.crudclientsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientsApiApplication.class, args);
+	}
+
+}
