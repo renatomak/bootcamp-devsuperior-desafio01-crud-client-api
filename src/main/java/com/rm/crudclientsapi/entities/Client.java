9275dae0c4206454478e3275a8747d371be87d2d
renatomak <renato.mark.silva@gmail.com>
2022-03-19 16:48:57 -0300

    feat: entity Client

---
+++ src/main/java/com/rm/crudclientsapi/entities/Client.java
@@ -0,0 +1,87 @@
+package com.rm.crudclientsapi.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String cpf;
+	
+	private Double income;
+	private Instant birthDate;
+	private Integer children;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Double getIncome() {
+		return income;
+	}
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+	public Integer getChildren() {
+		return children;
+	}
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", name=" + name + ", cpf=" + cpf + ", income=" + income + ", birthDate="
+				+ birthDate + ", children=" + children + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
